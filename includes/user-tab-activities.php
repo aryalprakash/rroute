07b4ed7bc62bbd83d8fd09736c036a4027d2f97c
sentiking <sentiraut@gmail.com>
2015-12-10 11:47:12 +0545

    remve url  from user-tab-activity.php

--- includes/user-tab-activities.php
+++ includes/user-tab-activities.php
@@ -16,8 +16,8 @@ if ($interactions){
 		$timestamp = $i['created_on'];
 		$user = getUserNameById($i['created_by']);
 		$author = getUserNameById($i['author']);
-		if($i['type'] == 'ideathread'){$title = getIdeaTitle($i['id']); $url =SITE_URL.'home.php?iid='.$i['id'];}
-		if($i['type'] == 'project'){$title = getProjectTitle($i['id']); $url = SITE_URL.'home.php?pid='.$i['id'];}
+		if($i['type'] == 'ideathread'){$title = getIdeaTitle($i['id']); $url =SITE_URL.'/home.php?iid='.$i['id'];}
+		if($i['type'] == 'project'){$title = getProjectTitle($i['id']); $url = SITE_URL.'/home.php?pid='.$i['id'];}
 		
 		if($i['action'] == 'like'){
 			$action = 'liked';
@@ -40,7 +40,7 @@ if ($interactions){
 		
 		<div style="font-size:15px; margin-bottom:10px;" >
         		<img src="<?php echo $icon_image?>" width="20px">
-        		<span class=""><?php echo $user.' '.$action.' '?><a href="<?php echo SITE_URL;?>/user.php?uid=<?php echo $i['author']?>" style="color: #FF0303; text-decoration:none;"><?php echo $author?></a><?php echo "'s ". $i['type']?> <a href="<?php echo $url?>" style="color: #FF0303; text-decoration:none;"><?php echo $title?>.<a></span>
+        		<span class=""><?php echo ucwords($user).' '.$action.' '?><a href="<?php echo SITE_URL;?>/user.php?uid=<?php echo  $i['author'];?>" style="color: #FF0303; text-decoration:none;"><?php echo ucwords($author);?></a><?php echo "'s ". $i['type']?> <a href="<?php echo $url?>" style="color: #FF0303; text-decoration:none;"><?php echo $title;?>.<a></span>
         		<span style="margin-left:10px; font-size:13px;"><?php echo TimeAgo(date('Y-m-d', strtotime($timestamp))); ?> </span>
         	</div>
 		
