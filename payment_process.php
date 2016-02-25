cbdb1d3f0980807d7fd09a32aa712b41a5d87170
sentiking <sentiraut@gmail.com>
2016-02-25 18:13:23 +0545

    payment Process tempfile

---
+++ payment_process.php
@@ -0,0 +1,29 @@
+<?php
+include('includes/header.php');
+
+require_once(DIR_APP . 'projects.php');
+require_once(DIR_APP . 'users.php');
+
+
+//print_r($_POST);
+
+$user_id =getProjectAuthor($_POST['pid']);
+$date =$_POST['month'].'/'.$_POST['year'];
+global $db_con;
+//checkBalance($user);
+
+$query = "INSERT INTO `payment_process`(`transaction_id`, `type`, `description`, `amount` , `user_id` , `balance` , `project_id`, `created_by`,`card_no`,`ccv`,`zip`,`expiry_date`)
+		 VALUES ( '" . $db_con->escape($_POST['stripeToken']) . "' , '".$db_con->escape($_POST['type'])."', 'Description' , '" . $db_con->escape($_POST['amount']) . "' , '" . $user_id. "' , '0' , '" . $db_con->escape($_POST['pid']) . "' , '" . $db_con->escape($_POST['user_id']) . "','" . $db_con->escape($_POST['cardname']) . "','" . $db_con->escape($_POST['cvc']) . "','" . $db_con->escape($_POST['zip_code']) . "','".$date."')";
+$db_con->query($query);
+$id = $db_con->insert_id();
+
+//$query1 = "UPDATE `users` SET balance=" . $balance . " WHERE user_id=" . $user['user_id'];
+//$db_con->query($query1);
+
+updateFundStatusProject($_POST['pid']);
+updateFundableStatus($_POST['pid']);
+updateFundingsStatus($_POST['pid'],$_POST['amount']);
+//return $id;
+echo "Payment Succesfull!";
+redirect('home.php');
+exit;
